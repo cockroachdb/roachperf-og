3b40a4f4fadea63d56a03b152a079af7796952c4
Jordan Lewis <jordanthelewis@gmail.com>
2017-11-29 10:41:42 -0500

    Update run-nightly to use roachprod

--- run-nightly.sh
+++ run-nightly.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+cd ../../cockroachlabs/roachprod
+go install
+cd ../../cockroachdb/roachperf
 go get -u ./...
 go install
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-182.0.0-linux-x86_64.tar.gz
+tar -zxf google-cloud-sdk-182.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+source google-cloud-sdk/path.bash.inc
+
+echo $GOOGLE_CREDENTIALS > creds.json
+
+gcloud auth activate-service-account --key-file=creds.json
+
+# It might already exist.
+roachprod -u teamcity create nightly || roachprod sync
+
 eval $(ssh-agent)
-ssh-add id_test_cluster.rsa
-mkdir -p ~/.ssh
-for i in {1..7}; do
-    ssh-keyscan -H cockroach-denim-000$i.crdb.io >> ~/.ssh/known_hosts
-done
+ssh-add ~/.ssh/google_compute_engine
 
 curl -L https://edge-binaries.cockroachdb.com/cockroach/cockroach.linux-gnu-amd64.LATEST -o cockroach
 chmod +x cockroach
+curl -L https://edge-binaries.cockroachdb.com/loadgen/kv.LATEST -o kv
+chmod +x kv
 
-roachperf denim put ./cockroach ./cockroach
+roachperf teamcity-nightly put ./cockroach ./cockroach
+roachperf teamcity-nightly put ./kv ./kv
 
 cd artifacts
-roachperf denim test nightly
+roachperf teamcity-nightly test nightly
 roachperf upload $(ls)
